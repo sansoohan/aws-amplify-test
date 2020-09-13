e13d7ffcde57aacf62acd8cd37a6b48868ee41e8
SanSoo Han <han@SanSoos-MacBook-Pro.local>
2020-09-14 00:04:54 +0900

    First Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import {API} from "aws-amplify";
 
 function App() {
+  const [petName, setPetName] = React.useState('')
+  const [petType, setPetType] = React.useState('')
+  const [pets, setPets] = React.useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    API.post('petsapi', '/pets', {
+      body: {
+        name: petName,
+        type: petType,
+      }
+    })
+    .then(() => {
+      setPets([{name: petName, type: petType}, ...pets])
+    })
+  }
+
+  useEffect(() => {
+    API.get('petsapi', '/pets/name')
+    .then(petRes => {
+      setPets([...pets, ...petRes])
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello
+        <form onSubmit={handleSubmit}>
+          <input value={petName} placeholder="fiddo" onChange={(e)=>setPetName(e.target.value)}/>
+          <input value={petType} placeholder="fiddo" onChange={(e)=>setPetType(e.target.value)}/>
+          <button>Add Pet</button>
+        </form>
+        <ul>
+          {pets.map(pet => <li>{pet.name}</li>)}
+        </ul>
+        <AmplifySignOut/>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
